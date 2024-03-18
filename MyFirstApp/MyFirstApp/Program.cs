0f98940ed48aeaca9690c780584527d79bc5fd6b
s26631 <163142945+s26631@users.noreply.github.com>
2024-03-18 17:51:27 +0100

    Update Program.cs

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,5 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Commit1");
-Console.WriteLine("Commit2");
-Console.WriteLine("Commit3");
+﻿static double Adavg(int[] numbers) {
+    double sum = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        sum += numbers[i];
+    }
+
+    return sum / numbers.Length;
+}
+int[] arr = {1,2,3,4,};
+double average = Adavg(arr);
+Console.WriteLine(average);
